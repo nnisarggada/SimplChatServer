d2f1c26ef04fe4cf3d8d4c55f3772d873f1a6ba4
nnisarggada <gadannisarg@gmail.com>
2023-07-02 23:23:36 +0530

    fixed cors

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
-const express = require('express');
-const http = require('http');
-const socketIO = require('socket.io');
-const cors = require('cors');
-
+const express = require("express");
 const app = express();
-const server = http.createServer(app);
-const io = socketIO(server);
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
 
-// Enable CORS with all origins allowed
 app.use(cors());
 
-io.on('connection', (socket) =>{
-  
+app.get("/wakeup", function (request, response) {
+  console.log("i'm awake");
+  response.send("i'm awake");
+});
+
+const server = http.createServer(app);
+
+const users = {};
+
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+io.on("connection", (socket) => {
   console.log(socket.id);
 
-  socket.on('join_room', (data) =>{
+  socket.on("join_room", (data) => {
     socket.join(data);
     console.log(`User ${socket.id} joined Room ${data}`);
   });
 
-  socket.on('send_message', (data) =>{
-    socket.to(data.room).emit('receive_message', data);
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
   });
 
-  socket.on('disconnect', ()=>{
+  socket.on("disconnect", () => {
     console.log("User disconnected", socket.id);
   });
 });
 
-const port = process.env.PORT || 3001;
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+server.listen(3000, () => {
+  console.log("SERVER RUNNING");
 });
